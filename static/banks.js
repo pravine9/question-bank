6699d85a4770605949540de25324f231fb6340b4
Pravine Siva <pravine@Pravines-MacBook-Pro.local>
2025-08-02 11:30:39 +0100

    removed duplicate question bank clinical therapeutics low

--- static/banks.js
+++ static/banks.js
@@ -7,7 +7,6 @@ import clinicalOtcLow from '../question_banks/clinical_otc_low_questions.js';
 import clinicalTherapeuticsHigh from '../question_banks/clinical_therapeutics_high_questions.js';
 import clinicalTherapeuticsLow from '../question_banks/clinical_therapeutics_low_questions.js';
 import clinicalTherapeuticsMedium from '../question_banks/clinical_therapeutics_medium_questions.js';
-import clinicalTherapeuticsQuestionsLow from '../question_banks/clinical_therapeutics_questions_low.js';
 
 export default {
   _info: "Populate the question_banks/ directory with JavaScript modules exporting question arrays before using the interface",
@@ -15,5 +14,5 @@ export default {
   clinical_mep: [clinicalMepLow],
   clinical_mixed: [clinicalMixedHigh, clinicalMixedLow, clinicalMixedMedium],
   clinical_otc: [clinicalOtcLow],
-  clinical_therapeutics: [clinicalTherapeuticsHigh, clinicalTherapeuticsLow, clinicalTherapeuticsMedium, clinicalTherapeuticsQuestionsLow]
+  clinical_therapeutics: [clinicalTherapeuticsHigh, clinicalTherapeuticsLow, clinicalTherapeuticsMedium]
 };
